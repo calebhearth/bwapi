bad9213f58f3ec0eb5050601ca07e0739d63968c
kovarex <kovarex@273ecf2c-f24b-0410-b6a7-e9298bb4873f>
2008-05-01 20:38:16 +0000

    Reformat
    git-svn-id: http://bwapi.googlecode.com/svn@23 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- bwapi/BW/BW_Unit.h
+++ bwapi/BW/BW_Unit.h
@@ -12,8 +12,8 @@ namespace BW_MovementFlags
 {
    enum Enum : u8
    {
-      MakingBuilding = 0x02,
-      Moving         = 0x04
+      MakingBuilding = 1 << 1,
+      Moving         = 1 << 2
    };
 };
 
@@ -21,19 +21,19 @@ namespace BW_OrderID
 {
   enum Enum : u8
   {
-    Idle = 3,
-    Moving = 6,
-    Attacking = 10,
-    AttackMoving = 14,
-    GoingToBuild = 31,
-    Following = 49,
-    ApproachingRafinery = 81,
-    EnteringRafinery = 82,
-    InRafinery = 83,
-    ReturningGas = 84,
-    ApproachingMinerals = 85,
-    Mining = 87,
-    ReturningMinerals = 90,
+    Idle                 = 3,
+    Moving               = 6,
+    Attacking            = 10,
+    AttackMoving         = 14,
+    GoingToBuild         = 31,
+    Following            = 49,
+    ApproachingRafinery  = 81,
+    EnteringRafinery     = 82,
+    InRafinery           = 83,
+    ReturningGas         = 84,
+    ApproachingMinerals  = 85,
+    Mining               = 87,
+    ReturningMinerals    = 90,
     GettingMinedMinerals = 151
   };
 }
