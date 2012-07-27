88660fb5a99d084539c1ad1686e93a83e0f5ea92
AHeinerm@gmail.com <AHeinerm@gmail.com@273ecf2c-f24b-0410-b6a7-e9298bb4873f>
2012-07-27 05:03:37 +0000

    [BWAPI4]  - Regionset::getCenter and getUnits  - Messing with documentation again.
    git-svn-id: http://bwapi.googlecode.com/svn@4165 273ecf2c-f24b-0410-b6a7-e9298bb4873f

--- branches/bwapi4/bwapi/BWAPILIB/Source/Regionset.cpp
+++ branches/bwapi4/bwapi/BWAPILIB/Source/Regionset.cpp
@@ -1,5 +1,7 @@
 #include <BWAPI/Vectorset.h>
 #include <BWAPI/Regionset.h>
+#include <BWAPI/Region.h>
+#include <BWAPI/Unitset.h>
 
 #include <utility>
 
@@ -9,6 +11,29 @@ namespace BWAPI
   Regionset::Regionset(const Regionset &other) : Vectorset(other) { };
   Regionset::Regionset(Regionset &&other) : Vectorset( std::forward<Regionset>(other) ) { };
 
+  Position Regionset::getCenter() const
+  {
+    Position sum; // The sum of all positions
+    int count = 0; // The number of valid positions
 
+    for ( auto r = this->begin(); r != this->end(); ++r )
+    {
+      Position p = r->getCenter();
+      if ( p )  // Only use if position is valid/known
+      {
+        sum += p;
+        ++count;
+      }
+    } // for
+    return sum /= count;
+  }
+
+  Unitset Regionset::getUnits(const UnitFilter &pred) const
+  {
+    Unitset units;
+    for ( auto r = this->begin(); r != this->end(); ++r )
+      units += r->getUnits(pred);
+    return units;
+  }
 }
 
